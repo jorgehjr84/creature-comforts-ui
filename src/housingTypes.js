86e898ab96a13a17cb7b0e0bb546271e19ee5706
Jorge Hernandez <jorgehjr84@gmail.com>
2024-11-02 11:25:50 -0400

    Dropdown is working in cost breakdown section

---
+++ src/housingTypes.js
@@ -0,0 +1,20 @@
+export const HOUSING_TYPES = [
+  {
+    key: "dome",
+    label: "Pacific Domes",
+    link: "https://pacificdomes.com/",
+    averageCost: 26200,
+  },
+  {
+    key: "yurt",
+    label: "Pacific Yurts",
+    link: "https://www.yurts.com/",
+    averageCost: 14120,
+  },
+  {
+    key: "boxabl",
+    label: "Boxabl",
+    link: "https://www.boxabl.com/",
+    averageCost: 60000,
+  },
+];
--- src/pages/CostBreakdown.jsx
+++ src/pages/CostBreakdown.jsx
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { ArrowLeftOutlined } from "@ant-design/icons";
+import { Button, Dropdown } from "antd";
+import boxabl from "../images/boxabl.png";
+import yurt from "../images/yurt.png";
+import dome from "../images/dome.png";
 
-import { Button } from "antd";
+import { HOUSING_TYPES } from "../housingTypes";
 
 export const CostBreakdown = () => {
+  const [housingType, setHousingType] = useState(null);
+  const [selectedImage, setSelectedImage] = useState(null);
+
+  const handleHousingTypeChange = (item) => {
+    setHousingType(item);
+
+    if (item.key === "boxabl") {
+      setSelectedImage(boxabl);
+    } else if (item.key === "yurt") {
+      setSelectedImage(yurt);
+    } else if (item.key === "dome") {
+      setSelectedImage(dome);
+    } else {
+      return item.imgUrl;
+    }
+  };
+
+  const items = HOUSING_TYPES.map((item) => {
+    return {
+      key: item.key,
+      label: <p onClick={() => handleHousingTypeChange(item)}>{item.label}</p>,
+    };
+  });
+
+  const formatCurrency = (amount) => {
+    return amount.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    });
+  };
+
   return (
     <div className="cost-breakdown-container">
       <div className="header">
@@ -14,7 +51,23 @@ export const CostBreakdown = () => {
           </Button>
         </Link>
       </div>
-      <h1>Cost Breakdown</h1>
+      <div className="content">
+        <Dropdown menu={{ items }} placement="bottomLeft">
+          <Button>Select your housing type</Button>
+        </Dropdown>
+
+        {housingType && (
+          <>
+            <div className="housing-info-container">
+              <h2>{housingType.label}</h2>
+              <p>Average cost: {formatCurrency(housingType.averageCost)}</p>
+              <div className="housing-image">
+                <img src={selectedImage} alt={housingType} />
+              </div>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 };
